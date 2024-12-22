6858722cb4e4340e4a3e0dc3dd4f3d82816b451d
Jeong Geumdam <>
2024-12-22 20:28:19 +0900

    Creating Effects: Modeling Synchronous Effects

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
-import { Effect, Console } from "effect";
+import { Effect } from "effect";
 
-const divide = (a: number, b: number): Effect.Effect<number, Error> => {
-    return b === 0 ? Effect.fail(new Error("Cannot divide by zero")) : Effect.succeed(a / b);
-}
-
-const res = divide(4, 0);
-// const program = Console.log(res);
-
+const divide = (a: number, b: number): Effect.Effect<number, Error> =>
+    b === 0
+        ? Effect.fail(new Error("Cannot divide by zero"))
+        : Effect.succeed(a / b);
 try {
-    const result = Effect.runSync(res);
-    console.log(result);
-}  catch (error) {
-    console.log(error);
+    console.log(Effect.runSync(divide(4, 0)));
+} catch (error) {
+    console.error(error);
 }
+
+interface User {
+    readonly id: number;
+    readonly name: string;
+}
+
+const getUser = (userId: number): Effect.Effect<User, Error> => {
+    const userDatabase: Record<number, User> = {
+        1: { id: 1, name: "John Doe" },
+        2: { id: 2, name: "Jane Smith" }
+    }
+
+    const user = userDatabase[userId];
+    if (user) {
+        return Effect.succeed(user);
+    } else {
+        return Effect.fail(new Error("User not found"));
+    }
+}
+
+const exampleUserEffect = getUser(1);
+console.log(Effect.runSync(exampleUserEffect));
+
+const log = (message: string) => {
+    return Effect.sync(() => {
+        console.log(message);
+    })
+}
+
+const program1 = log('hello world!');
+Effect.runSync(program1);
+
+const parse = (input: string) =>
+    Effect.try({
+        // JSON.parse may throw for bad input
+        try: () => JSON.parse(input),
+        // remap the error
+        catch: (unknown) => new Error(`something went wrong ${unknown}`)
+    })
+
+//      ┌─── Effect<any, Error, never>
+//      ▼
+const program2 = parse("");
+Effect.runSync(program2);
